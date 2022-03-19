4bf386951bfbabc6fda605a90570695ce12c47c8
r0ngsh3n <r0ngsh3n@gmail.com>
2022-03-19 10:34:20 -0400

    git init

---
+++ src/main/java/com/r0ngsh3n/mdcheatsheets/MarkDownCheatSheetsApplication.java
@@ -0,0 +1,13 @@
+package com.r0ngsh3n.mdcheatsheets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkDownCheatSheetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkDownCheatSheetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/r0ngsh3n/mdcheatsheets/MarkDownCheatSheetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.r0ngsh3n.mdcheatsheets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkDownCheatSheetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
